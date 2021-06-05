09cd042ef13700cf1d7d5dd71dcbde55510e3318
mariusaee <mariusae@me.com>
2021-06-05 12:06:39 +0300

    Initial Commit

---
+++ rgbColorMixer SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rgbColorMixer SwiftUI
+//
+//  Created by Marius Malyshev on 05.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rgbColorMixer SwiftUI/rgbColorMixer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  rgbColorMixer_SwiftUIApp.swift
+//  rgbColorMixer SwiftUI
+//
+//  Created by Marius Malyshev on 05.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct rgbColorMixer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
