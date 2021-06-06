8af1989e5daf2ecee0b79f24de4b0074f5625c66
mariusaee <mariusae@me.com>
2021-06-06 13:07:09 +0300

    add formatter

--- rgbColorMixer SwiftUI/ContentView.swift
+++ rgbColorMixer SwiftUI/ContentView.swift
@@ -7,28 +7,36 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct testView: View {
     
     @State private var redValue = Double.random(in: 0...1)
     @State private var greenValue = Double.random(in: 0...1)
     @State private var blueValue = Double.random(in: 0...1)
     @State private var sliderValue = Double.random(in: 0...255)
-    @State private var stringValue = "200"
+    
+    private let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .none
+        return formatter
+    }()
 
     var body: some View {
         
+                
         ZStack {
             Color(red: 32/255, green: 88/255, blue: 154/255)
                 .ignoresSafeArea()
             
             VStack(spacing: 40) {
-                ColoredView(redValue: $redValue, greenValue: $greenValue, blueValue: $blueValue)
+                ColoredView(redValue: $redValue,
+                            greenValue: $greenValue,
+                            blueValue: $blueValue)
                 HStack {
                     Text("\(lround(sliderValue))")
                         .foregroundColor(.white)
                         .frame(width: 50)
                     ColorSlider(value: $sliderValue, color: .red)
-                    TextField("", text: $stringValue)
+                    TextField("", value: $sliderValue, formatter: formatter)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .frame(width: 60)
                         .multilineTextAlignment(.trailing)
@@ -43,7 +51,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        testView()
     }
 }
 
